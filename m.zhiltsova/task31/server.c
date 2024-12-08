5fd1b348c29e20ee24ea5d243bc1ce7dcdaeff53
m-zhiltsova <m.zhiltsova@g.nsu.ru>
2024-12-08 18:05:24 +0700

    Add files via upload

--- m.zhiltsova/task31/server.c
+++ m.zhiltsova/task31/server.c
@@ -1,100 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <ctype.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
-#include <sys/types.h>
+#include <sys/select.h>
 
 #define SOCKET_PATH "unix_socket"
 #define BUFFER_SIZE 256
+#define MAX_CLIENTS 10
 
-volatile sig_atomic_t active_clients = 0;
-
-void handle_sigint(int sig) {
-    printf("Received SIGINT, shutting down server...\n");
-    exit(0);
+void to_uppercase(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = toupper((unsigned char)str[i]);
+    }
 }
 
 int main() {
-    int server_socket, client_socket;
-    struct sockaddr_un server_addr, client_addr;
-    socklen_t client_len;
+    int server_fd, client_fd, max_fd;
+    int active_clients = 0;
+    struct sockaddr_un server_addr;
     char buffer[BUFFER_SIZE];
+    fd_set read_fds, active_fds;
 
-    signal(SIGINT, handle_sigint);
-
-    server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("socket");
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    unlink(SOCKET_PATH);
-
-    memset(&server_addr, 0, sizeof(struct sockaddr_un));
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sun_family = AF_UNIX;
     strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    unlink(SOCKET_PATH); 
 
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("bind");
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Bind failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_socket, 5) < 0) {
-        perror("listen");
+    if (listen(server_fd, MAX_CLIENTS) == -1) {
+        perror("Listen failed");
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
+    printf("Server listening on %s\n", SOCKET_PATH);
 
-    fd_set read_fds;
-    int max_fd;
+    FD_ZERO(&active_fds);
+    FD_SET(server_fd, &active_fds);
+    max_fd = server_fd;
 
     while (1) {
-        FD_ZERO(&read_fds);
-        FD_SET(server_socket, &read_fds);
-        max_fd = server_socket;
+        read_fds = active_fds;
 
         if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
-            perror("select");
-            exit(EXIT_FAILURE);
+            perror("Select failed");
+            break;
         }
 
-        if (FD_ISSET(server_socket, &read_fds)) {
-            client_len = sizeof(client_addr);
-            client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
-            if (client_socket < 0) {
-                perror("accept");
-                continue;
-            }
-
-            active_clients++;
-
-            if (fork() == 0) {
-                close(server_socket);
-                int n;
-                while ((n = read(client_socket, buffer, sizeof(buffer))) > 0) {
-                    buffer[n] = '\0';
-                    for (int i = 0; i < n; i++) {
-                        buffer[i] = toupper(buffer[i]);
+        for (int i = 0; i <= max_fd; i++) {
+            if (FD_ISSET(i, &read_fds)) {
+                if (i == server_fd) {
+                    if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
+                        perror("Accept failed");
+                        continue;
                     }
-
-                    printf("Received: %s", buffer);
+                    printf("New client connected\n");
+                    FD_SET(client_fd, &active_fds);
+                    if (client_fd > max_fd) max_fd = client_fd;
+                    active_clients++;
+                } else {
+                    memset(buffer, 0, BUFFER_SIZE);
+                    ssize_t bytes_read = read(i, buffer, BUFFER_SIZE - 1);
+                    if (bytes_read > 0) {
+                        to_uppercase(buffer);
+                        printf("Client: %s\n", buffer);
+                    } else {
+                        if (bytes_read == 0) {
+                            printf("One client disconnected\n", i);
+                        } else {
+                            perror("Read failed");
+                        }
+                        close(i);
+                        FD_CLR(i, &active_fds);
+                        active_clients--;
+                    }
                 }
-                close(client_socket);
-                active_clients--;
-
-                if (active_clients == 0) {
-                    printf("All clients disconnected, shutting down server...\n");
-                    exit(0);
-                }
-                exit(0);
             }
-            close(client_socket);
+        }
+
+        if (active_clients == 0) {
+            printf("All clients disconnected. Server shutting down.\n");
+            break;
         }
     }
 
+    close(server_fd);
     unlink(SOCKET_PATH);
     return 0;
 }
